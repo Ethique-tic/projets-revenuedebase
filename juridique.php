9b033eadce048aea6a5e9669388a37b4ec07da46
Mohamed <momo.telecom@outlook.com>
2016-06-14 19:56:28 +0200

    Mise a jour de la page du point de vue juridique (corection titre + code)

--- juridique.php
+++ juridique.php
@@ -31,28 +31,33 @@
 
       <div class=".col-xs-12" style="text-align:justify;"> 
       
-      <h2>Résumé Point de vue Juridique</h2>
+      <h2>Le point de vue Juridique</h2>
 
-<p>
+        <p>
         Les motivations des promoteurs de cette controverse reposent pour l’essentiel sur la nécessité de dignité pour chacun et sur les droits de l’homme bien que le statut de la dignité soit en cours de création et un peu confus. En effet, la Déclaration Universelle des Droits de l’Homme de 1948 pose le revenu de base pour tous comme un droit fondamental.      
         </p>
+
         <p>
-« Toute personne a droit à un niveau de vie suffisant pour assurer sa santé, son bien-être et ceux de sa famille, notamment pour l'alimentation, l'habillement, le logement, les soins médicaux ainsi que pour les services sociaux nécessaires ; elle a droit à la sécurité en cas de chômage, de maladie, d'invalidité, de veuvage, de vieillesse ou dans les autres cas de perte de ses moyens de subsistance par suite de circonstances indépendantes de sa volonté.» (Article 25 de la Déclaration Universelle des Droits de l’Homme, 1948)
-Le droit à un revenu pour subvenir à ses besoins élémentaires est donc ici inaliénable.
-</p>
-<p>
-Cependant, le fait de donner à chaque citoyen les moyens vivre leur laisse le choix de travailler ou non. L'article 5 du préambule de la constitution de 1946 créant la 4ème République, et auquel se réfère encore notre actuelle constitution, énonce ceci : « Chacun a le devoir de travailler et le droit d'obtenir un emploi. (…) »
-</p>
-<p>
-L’allocation universelle serait ici anticonstitutionnelle ou alors il faudrait redéfinir la notion de travail. Muni de ce revenu inconditionnel, les citoyens n’auront plus la nécessité de travailler pour subvenir à leurs besoins ( primaires certes..).  Ainsi, ce projet de loi pourrait être censuré par le conseil constitutionnel.
-Aujourd’hui, cette controverse est de plus en plus débattue. Plusieurs propositions pour l’instauration d’un revenu de base ont été soumises au sénat mais aucune n’a encore été adoptée.
-</p>
-<p>
- Le jeudi 19 mai 2016, le sénat a chargé 27 sénateurs de tout bord de réaliser une mission d’information sur le revenu de base et de formuler des propositions. Le rapport doit être rendu dans 6 mois  et donnera la possibilité ou non de cette instauration.
- </p>
-<p>
-Pour l’instant, la mise en place d’un revenu de base semble prématurée mais dans un futur proche il sera probablement envisageable.
-</p>
+        « Toute personne a droit à un niveau de vie suffisant pour assurer sa santé, son bien-être et ceux de sa famille, notamment pour l'alimentation, l'habillement, le logement, les soins médicaux ainsi que pour les services sociaux nécessaires ; elle a droit à la sécurité en cas de chômage, de maladie, d'invalidité, de veuvage, de vieillesse ou dans les autres cas de perte de ses moyens de subsistance par suite de circonstances indépendantes de sa volonté.» (Article 25 de la Déclaration Universelle des Droits de l’Homme, 1948)
+        Le droit à un revenu pour subvenir à ses besoins élémentaires est donc ici inaliénable.
+        </p>
+
+        <p>
+        Cependant, le fait de donner à chaque citoyen les moyens vivre leur laisse le choix de travailler ou non. L'article 5 du préambule de la constitution de 1946 créant la 4ème République, et auquel se réfère encore notre actuelle constitution, énonce ceci : « Chacun a le devoir de travailler et le droit d'obtenir un emploi. (…) »
+        </p>
+
+        <p>
+        L’allocation universelle serait ici anticonstitutionnelle ou alors il faudrait redéfinir la notion de travail. Muni de ce revenu inconditionnel, les citoyens n’auront plus la nécessité de travailler pour subvenir à leurs besoins ( primaires certes..).  Ainsi, ce projet de loi pourrait être censuré par le conseil constitutionnel.
+        Aujourd’hui, cette controverse est de plus en plus débattue. Plusieurs propositions pour l’instauration d’un revenu de base ont été soumises au sénat mais aucune n’a encore été adoptée.
+        </p>
+
+        <p>
+         Le jeudi 19 mai 2016, le sénat a chargé 27 sénateurs de tout bord de réaliser une mission d’information sur le revenu de base et de formuler des propositions. Le rapport doit être rendu dans 6 mois  et donnera la possibilité ou non de cette instauration.
+        </p>
+
+        <p>
+          Pour l’instant, la mise en place d’un revenu de base semble prématurée mais dans un futur proche il sera probablement envisageable.
+        </p>
 
       </div>
        <!-- footer -->
